b8f2cf7b324464af6633e3c7bd2ee10ff95c0465
xvaldetaro <xvaldetaro@gmail.com>
2011-02-15 16:46:38 -0200

    Added ASSERT_DOUBLE_EQ and changed ASSERT_EQUAL to ASSERT_EQ

--- modules/testkit/Unit.lua
+++ modules/testkit/Unit.lua
@@ -29,7 +29,7 @@ function ASSERT_TRUE( condition, message )
 	if not condition then tError( message, "The condition should be true, but it actually was false" ) end
 end
 
-function ASSERT_EQUALS( value, expected, message )
+function ASSERT_EQ( value, expected, message )
 	value = normalizeValue( value )
 	expected = normalizeValue( expected )
 	--if type(value) ~= type(expected) then error( "compared values are two different types." ) end
@@ -37,23 +37,33 @@ function ASSERT_EQUALS( value, expected, message )
 		tError( message, "got " .. tostring(value) .. " when expecting " .. expected )
 	end
 end
-
+function ASSERT_DOUBLE_EQ( value, expected, message )
+	value = normalizeValue( value )
+	expected = normalizeValue( expected )
+	largerAbsolute = math.abs( ( ( value > expected ) and value ) or expected )
+	delta = math.abs( ( math.abs( value ) - math.abs( expected ) ) )
+	tolerance = ( ( largerAbsolute < 0.0001 ) and 0.0000001 ) or largerAbsolute * 0.0000001 
+	
+ 	if  delta >  tolerance then
+		tError( message, "got " .. tostring(value) .. " when expecting " .. expected )
+	end
+end
 function ASSERT_VECTOR2( vector1, vector2 )
-	ASSERT_EQUALS( vector1.x, vector2.x, "The value of coordinate X is " .. tostring(vector1.x) .. " when the expected is " .. tostring(vector2.x) )
-	ASSERT_EQUALS( vector1.y, vector2.y, "The value of coordinate Y is " .. tostring(vector1.y) .. " when the expected is " .. tostring(vector2.y) )
+	ASSERT_EQ( vector1.x, vector2.x, "The value of coordinate X is " .. tostring(vector1.x) .. " when the expected is " .. tostring(vector2.x) )
+	ASSERT_EQ( vector1.y, vector2.y, "The value of coordinate Y is " .. tostring(vector1.y) .. " when the expected is " .. tostring(vector2.y) )
 end
 
 function ASSERT_VECTOR3( vector1, vector2 )
-	ASSERT_EQUALS( vector1.x, vector2.x, "The value of coordinate X is " .. tostring(vector1.x) .. " when the expected is " .. tostring(vector2.x) )
-	ASSERT_EQUALS( vector1.y, vector2.y, "The value of coordinate Y is " .. tostring(vector1.y) .. " when the expected is " .. tostring(vector2.y) )
-	ASSERT_EQUALS( vector1.z, vector2.z, "The value of coordinate Z is " .. tostring(vector1.z) .. " when the expected is " .. tostring(vector2.z) )
+	ASSERT_EQ( vector1.x, vector2.x, "The value of coordinate X is " .. tostring(vector1.x) .. " when the expected is " .. tostring(vector2.x) )
+	ASSERT_EQ( vector1.y, vector2.y, "The value of coordinate Y is " .. tostring(vector1.y) .. " when the expected is " .. tostring(vector2.y) )
+	ASSERT_EQ( vector1.z, vector2.z, "The value of coordinate Z is " .. tostring(vector1.z) .. " when the expected is " .. tostring(vector2.z) )
 end
 
 function ASSERT_VECTOR4( vector1, vector2 )
-	ASSERT_EQUALS( vector1.x, vector2.x, "The value of coordinate X is " .. tostring(vector1.x) .. " when the expected is " .. tostring(vector2.x) )
-	ASSERT_EQUALS( vector1.y, vector2.y, "The value of coordinate Y is " .. tostring(vector1.y) .. " when the expected is " .. tostring(vector2.y) )
-	ASSERT_EQUALS( vector1.z, vector2.z, "The value of coordinate Z is " .. tostring(vector1.z) .. " when the expected is " .. tostring(vector2.z) )
-	ASSERT_EQUALS( vector1.w, vector2.w, "The value of coordinate W is " .. tostring(vector1.w) .. " when the expected is " .. tostring(vector2.w) )
+	ASSERT_EQ( vector1.x, vector2.x, "The value of coordinate X is " .. tostring(vector1.x) .. " when the expected is " .. tostring(vector2.x) )
+	ASSERT_EQ( vector1.y, vector2.y, "The value of coordinate Y is " .. tostring(vector1.y) .. " when the expected is " .. tostring(vector2.y) )
+	ASSERT_EQ( vector1.z, vector2.z, "The value of coordinate Z is " .. tostring(vector1.z) .. " when the expected is " .. tostring(vector2.z) )
+	ASSERT_EQ( vector1.w, vector2.w, "The value of coordinate W is " .. tostring(vector1.w) .. " when the expected is " .. tostring(vector2.w) )
 end
 
 function EXPECT_EXCEPTION( errorMessage, func, ...  )
