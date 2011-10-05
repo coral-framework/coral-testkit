c52d210e4f23cda74e01cab9d39dc026ee8bb0ae
Gabriel Rossi <papumpererepaumdoce@gmail.com>
2011-10-05 12:52:43 -0300

    added memory query for linux so

--- src/MemoryQuery.cpp
+++ src/MemoryQuery.cpp
@@ -6,6 +6,16 @@
 #include <psapi.h>
 #endif
 
+#if defined( CORAL_OS_LINUX )
+#include <time.h>
+#include <sys/times.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#endif
+
+
 #include "MemoryQuery_Base.h"
 
 namespace testkit {
@@ -38,6 +48,20 @@ public:
 
 		GetProcessMemoryInfo( hProcess, &pmc, sizeof(pmc));
 		return static_cast<co::int32>( pmc.WorkingSetSize );
+		#endif
+		#if defined( CORAL_OS_LINUX )
+		char buf[30];
+		snprintf(buf, 30, "/proc/%u/statm", (unsigned)getpid());
+		FILE* pf = fopen(buf, "r");
+		if (pf) {
+			unsigned size; //       total program size
+			unsigned resident;//   resident set size
+			fscanf(pf, "%u %u" /* %u %u %u %u"*/, &size, &resident /*, &share, &text, &lib, &data*/);
+			fclose(pf);
+			return static_cast<co::int32>( size );
+		}
+		fclose(pf);
+		return 0;
 		#endif
 		return 0;
 	}
